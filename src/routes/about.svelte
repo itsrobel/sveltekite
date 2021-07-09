f298319e2035866539080e6c2cd85b28fcee0fb0
Robel Schwarz <itsrobel.schwarz@gmail.com>
2021-07-09 18:40:08 -0400

    update/fixes

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,7 +5,7 @@
 	import Typewriter from 'svelte-typewriter';
 
 	let open = false;
-	let links = ['about', 'live demos', 'resume'];
+	let links = ['about', 'demos', 'resume'];
 
 	function handleClick() {
 		open = !open;
@@ -19,13 +19,14 @@
 				<div class="flex items-center justify-between ">
 					<div class="flex items-center">
 						<div class="flex-shrink-0">
-							<button
+							<!-- <button
 								type="button"
 								class="max-w-xs  rounded-full flex items-center text-sm focus:outline-none  "
 								id="user-menu-button"
 								aria-expanded="false"
 								aria-haspopup="true"
 								on:click={handleClick}
+								
 							>
 								<svg
 									width="24"
@@ -33,6 +34,8 @@
 									viewBox="0 0 68 68"
 									fill="black"
 									xmlns="http://www.w3.org/2000/svg"
+									
+									
 								>
 									<path
 										d="M27.0001 5.99992L21.0001 7.99992L16.0001 11.4999L11 16.4999L7.49999 22.4999L5.5 27.9999L5 32.4999V35.9999L5.5 38.9999L7.49999 45.4999L11 51.4999L16.0001 56.4999L20.5 59.4999L26 62L34 63.5H39.5L48.5 61L52.5 57L47 58.5L37.5 59.4999L30 59.1095L25.5 56.4999L20.5 52.5L16.0001 47.5L12.5 41.5L11 34L16.0001 27.9999L22 33L24.5 22L28 16.4999L34 11.4999L47.5 7.99992C45.8397 7.12303 39.5799 5.79794 34 4.99992H31.5001L27.0001 5.99992ZM42.5 30.9999L36 37.4999L42.5 43.9999L48.5 37.4999L42.5 30.9999ZM37.5 32.4999H47.5V42.4999H37.5V32.4999Z"
@@ -40,18 +43,14 @@
 										stroke-width="9"
 									/>
 								</svg>
-								<a
-									class="text-xl font-bold text-white transition-colors duration-300 transform md:text-2xl hover:text-indigo-400"
-									href="/">Amadeus</a
-								>
-							</button>
+							</button> -->
 						</div>
 						<div class="hidden md:block">
 							<div class="ml-10 flex items-baseline space-x-4">
 								{#each links as link}
 									<a
-										href="/{link}"
-										class="text-xl font-bold text-white transition-colors duration-300 transform md:text-2xl hover:text-indigo-400"
+										href="#{link}"
+										class="text-xl font-bold text-white transition-colors duration-300 transform md:text-2xl hover:text-pink-400"
 										>{link.toUpperCase()}</a
 									>
 								{/each}
@@ -62,7 +61,7 @@
 			</div>
 		</nav>
 
-		{#if open}
+		<!-- {#if open}
 			<div
 				in:fly={{ x: -300, duration: 2000 }}
 				out:fly={{ x: -300, duration: 2000 }}
@@ -79,64 +78,42 @@
 					{/each}
 				</div>
 				<div class="pt-4 pb-3 border-t border-gray-700">
-					<div class="flex items-center px-5">
-						<div class="flex-shrink-0" />
-						<div class="ml-3" />
-						<button
-							class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-						>
-							<span class="sr-only">View notifications</span>
-							<svg
-								class="h-6 w-6"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-								/>
-							</svg>
-						</button>
-					</div>
+					
 					<div class="mt-3 px-2 space-y-1" />
 				</div>
 			</div>
-		{/if}
-		<section class="flex items-center justify-center" style="height: 500px;">
+		{/if} -->
+		<section class="flex items-center justify-center wall" style="height: 500px; ">
 			<div class="text-center">
-				<p class="text-xl font-medium tracking-wider text-gray-300">Lorem ipsum dolor</p>
-				<h2 class="mt-6 text-3xl font-bold text-white md:text-5xl">
-					Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit
+				<h2 class="mt-6 text-3xl font-bold text-white md:text-5xl hover:text-yellow-300">
+					Custom  And Prebulit  <br />Functional Web Images
 				</h2>
+				<p class="text-xl font-medium tracking-wider text-gray-200 hover:text-yellow-300">Utilize The Power of Docker To scale your Website Now</p>
 
 				<div class="flex justify-center mt-8">
 					<a
-						class="px-8 py-2 text-lg font-medium text-white transition-colors duration-300 transform bg-indigo-600 rounded hover:bg-indigo-500"
-						href="#">Get In Touch</a
+						class="px-8 py-2 text-lg font-medium text-white transition-colors duration-300 transform bg-gray-900 rounded hover:bg-pink-400"
+						href="#">Make An Order Now</a
 					>
 				</div>
 			</div>
 		</section>
 	</header>
 
-	<section class="bg-white">
+	<section class="bg-gray-900">
 		<div class="max-w-5xl px-6 py-16 mx-auto">
 			<div class="items-center md:flex md:space-x-6">
 				<div class="md:w-1/2">
-					<h3 class="text-2xl font-semibold text-gray-800">
-						Lorem ipsum dolor sit <br /> amet, consectetur
+					<h3 class="text-2xl font-semibold text-pink-400">
+						Why You?
 					</h3>
-					<p class="max-w-md mt-4 text-gray-600">
-						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-						officia deserunt mollit anim id est laborum.
+					<p class="max-w-md mt-4 text-gray-400 hover:text-yellow-300">
+						Amadeus as a company is run and owned by one person.
+						 The clear down side is that the proccsess for your order will be slower,
+						  right? since this is a one man operation contact is a high priority,
+						  my direct contact line insures faster and more consistant product updates,
+						  with no middle man to get in the way.
 					</p>
-					<a href="#" class="block mt-8 text-indigo-700 underline">Experienced team</a>
 				</div>
 
 				<div class="mt-8 md:mt-0 md:w-1/2">
@@ -154,7 +131,7 @@
 		</div>
 	</section>
 
-	<section class="bg-white">
+	<section class="bg-white wall2" id="about">
 		<div class="max-w-5xl px-6 py-16 mx-auto">
 			<div class="items-center md:flex md:space-x-6">
 				<div class="md:w-1/2">
@@ -170,29 +147,29 @@
 				</div>
 
 				<div class="mt-8 md:mt-0 md:w-1/2">
-					<h3 class="text-2xl font-semibold text-gray-800">
-						Lorem ipsum dolor sit <br /> amet, consectetur
+				<div class="px-6 py-8 overflow-hidden bg-white rounded-md shadow-md bg-gray-900">
+					<h3 class="text-2xl font-semibold text-pink-400">
+						Who Are You?
 					</h3>
-					<p class="max-w-md mt-4 text-gray-600">
-						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-						officia deserunt mollit anim id est laborum.
+					<p class="max-w-md mt-4 text-gray-300 hover:text-yellow-300">
+						My name is Robel Schwarz and I am a software devoloper that specializes in making websites functional and more user interactive.
+						 I started coding from a young age and have used many different techonlogies since I started.
+						 I push myself to be a better programmer and a better man to help the those who arent as fortunate as I am
 					</p>
-					<a href="#" class="block mt-8 text-indigo-700 underline">Experienced team</a>
+				</div>
 				</div>
 			</div>
 		</div>
 	</section>
 
-	<section class="bg-white">
+	<section class="bg-gray-900">
 		<div class="max-w-5xl px-6 py-16 mx-auto">
-			<h2 class="text-3xl font-semibold text-gray-800">
-				Lorem ipsum dolor sit amet, <br /> consectetur adipiscing
+			<h2 class="text-3xl font-semibold text-pink-400">
+				The Goal
 			</h2>
-			<p class="max-w-lg mt-4 text-gray-600">
-				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-				pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.
+			<p class="max-w-lg mt-4 text-gray-300 hover:text-yellow-300">
+				To create a system of self improvement and learning funded by freelance work.
+				 I wish to create content around my daily life to impove myself and the people who choose to follow me
 			</p>
 			<div class="grid gap-8 mt-10 md:mt-20 md:grid-cols-2">
 				<div class="flex space-x-4">
@@ -210,10 +187,9 @@
 					</svg>
 
 					<div>
-						<h4 class="text-xl font-medium text-gray-800">Design concept</h4>
-						<p class="max-w-lg mt-4 text-gray-600">
-							Vitae nulla nunc euismod vel nunc euismod velpretium tellus accumsan nulla nunc
-							euismod ve semper.
+						<h4 class="text-xl font-medium text-pink-400 ">Social concept</h4>
+						<p class="max-w-lg mt-4 text-gray-300 hover:text-yellow-300">
+							A series of youtube video and shorts to push the brand forward so that I may purse bigger opporitinities to help others 
 						</p>
 					</div>
 				</div>
@@ -233,11 +209,9 @@
 					</svg>
 
 					<div>
-						<h4 class="text-xl font-medium text-gray-800">Developing websites</h4>
-						<p class="max-w-lg mt-4 text-gray-600">
-							Vitae nulla euismod velpretium tellus accumsan nulla nunc euismod ve semper. Vitae
-							nulla euismod velpretium tellus accumsan nulla nunc euismod ve semper. Vitae nulla
-							euismod velpretium tellus accumsan nulla nunc euismod ve semper.
+						<h4 class="text-xl font-medium text-pink-400">Developing websites</h4>
+						<p class="max-w-lg mt-4 text-gray-300 hover:text-yellow-300 ">
+							A interest of me that devolped into a full time hobby that I realized could be very rewarding finacially and socially
 						</p>
 					</div>
 				</div>
@@ -245,16 +219,15 @@
 		</div>
 	</section>
 
-	<section class="bg-white">
+	<!-- <section class="bg-white">
 		<div class="max-w-5xl px-6 py-16 mx-auto">
 			<div
 				class="px-8 py-12 bg-gray-800 rounded-md md:px-20 md:flex md:items-center md:justify-between"
 			>
 				<div>
-					<h3 class="text-2xl font-semibold text-white">Lorem ipsum dolor sit amet</h3>
+					<h3 class="text-2xl font-semibold text-white">Get in touch now</h3>
 					<p class="max-w-md mt-4 text-gray-400">
-						Lorem ipsum dolor sit amet, consectetur adipiscing Ac aliquam ac volutpat, viverra magna
-						risus aliquam massa.
+						to push your company and my dream forward into the future
 					</p>
 				</div>
 
@@ -264,37 +237,36 @@
 				>
 			</div>
 		</div>
-	</section>
+	</section> -->
 
-	<section class="bg-white">
+	<section class="bg-white wall3" id="demos">
 		<div class="max-w-5xl px-6 py-16 mx-auto">
 			<div class="md:flex md:justify-between">
-				<h2 class="text-3xl font-semibold text-gray-800">
-					Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod
+				<h2 class="text-3xl font-semibold text-pink-400">
+					Live Demos
 				</h2>
-				<a href="#" class="block mt-6 text-indigo-700 underline md:mt-0">Experienced team</a>
 			</div>
 
 			<div class="grid gap-8 mt-10 md:grid-cols-2 lg:grid-cols-3">
-				<div class="px-6 py-8 overflow-hidden bg-white rounded-md shadow-md">
-					<h2 class="text-xl font-medium text-gray-800">Audio</h2>
-					<p class="max-w-md mt-4 text-gray-400">
+				<div class="px-6 py-8 overflow-hidden bg-white rounded-md shadow-md bg-gray-900">
+					<h2 class="text-xl font-medium text-pink-400">Audio</h2>
+					<p class="max-w-md mt-4 text-gray-300 hover:text-yellow-300">
 						Lorem ipsum dolor sit amet, consectetur adipiscing Ac aliquam ac volutpat, viverra magna
 						risus aliquam massa.
 					</p>
 				</div>
 
-				<div class="px-6 py-8 overflow-hidden bg-white rounded-md shadow-md">
-					<h2 class="text-xl font-medium text-gray-800">Audio</h2>
-					<p class="max-w-md mt-4 text-gray-400">
+				<div class="px-6 py-8 overflow-hidden bg-white rounded-md shadow-md bg-gray-900">
+					<h2 class="text-xl font-medium text-pink-400">Audio</h2>
+					<p class="max-w-md mt-4 text-gray-300 hover:text-yellow-300">
 						Lorem ipsum dolor sit amet, consectetur adipiscing Ac aliquam ac volutpat, viverra magna
 						risus aliquam massa.
 					</p>
 				</div>
 
-				<div class="px-6 py-8 overflow-hidden bg-white rounded-md shadow-md">
-					<h2 class="text-xl font-medium text-gray-800">Audio</h2>
-					<p class="max-w-md mt-4 text-gray-400">
+				<div class="px-6 py-8 overflow-hidden bg-white rounded-md shadow-md bg-gray-900">
+					<h2 class="text-xl font-medium text-pink-400">Audio</h2>
+					<p class="max-w-md mt-4 text-gray-300 hover:text-yellow-300">
 						Lorem ipsum dolor sit amet, consectetur adipiscing Ac aliquam ac volutpat, viverra magna
 						risus aliquam massa.
 					</p>
@@ -303,15 +275,13 @@
 		</div>
 	</section>
 
-	<section class="bg-white">
+	<section class="bg-gray-900" id="resume">
 		<div class="max-w-5xl px-6 py-16 mx-auto text-center">
-			<h2 class="text-3xl font-semibold text-gray-800">
-				Lorem ipsum dolor sit amet, <br /> consectetur adipiscing
+			<h2 class="text-3xl font-semibold text-pink-400 ">
+				Resume
 			</h2>
-			<p class="max-w-lg mx-auto mt-4 text-gray-600">
-				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-				pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.
+			<p class="max-w-lg mx-auto mt-4 text-gray-300 hover:text-yellow-300">
+				If your company is interested in hiring for a possition that would be more of a commitment than freelance feel free in reading my resume
 			</p>
 
 			<img
@@ -321,7 +291,7 @@
 		</div>
 	</section>
 
-	<section class="bg-white">
+	<section class="bg-white wall4">
 		<div class="max-w-5xl px-6 py-16 mx-auto space-y-8 md:flex md:items-center md:space-y-0">
 			<div class="md:w-2/3">
 				<div class="hidden md:flex md:items-center md:space-x-10">
@@ -334,14 +304,12 @@
 						src="https://images.unsplash.com/photo-1618506469810-282bef2b30b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
 					/>
 				</div>
-				<h2 class="text-3xl font-semibold text-gray-800 md:mt-6">Lorem ipsum dolor</h2>
-				<p class="max-w-lg mt-4 text-gray-600">
-					Duis aute irure dolor in reprehenderit in voluptate velit esse illum dolore eu fugiat
-					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-					deserunt mollit anim id est laborum dolore eu fugiat nulla pariatur. Excepteur sint
-					occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-					laborum.
+				<div class="px-6 py-8 overflow-hidden bg-white rounded-md shadow-md bg-gray-900 md:mt-6">
+				<h2 class="text-3xl font-semibold text-pink-400 ">Expanding</h2>
+				<p class="max-w-lg mt-4 text-gray-300 hover:text-yellow-300">
+					Over the course of the next couple of years I am looking into expanding my skill set to other fields incluing robotics , 3d desgin , and computer vision.
 				</p>
+			</div>
 			</div>
 			<div class="md:w-1/3">
 				<img
@@ -353,13 +321,11 @@
 		</div>
 	</section>
 
-	<section class="bg-white">
+	<!-- <section class="bg-white">
 		<div class="max-w-5xl px-6 py-16 mx-auto text-center">
-			<h2 class="text-3xl font-semibold text-gray-800">Our Leadership</h2>
+			<h2 class="text-3xl font-semibold text-gray-800">More From Me</h2>
 			<p class="max-w-lg mx-auto mt-4 text-gray-600">
-				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-				pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.
+				
 			</p>
 
 			<div class="grid gap-8 mt-6 md:grid-cols-2 lg:grid-cols-4">
@@ -400,12 +366,34 @@
 				</div>
 			</div>
 		</div>
-	</section>
+	</section> -->
 
-	<footer class="border-t">
+	<footer class="bg-gray-900">
 		<div class="container flex items-center justify-between px-6 py-8 mx-auto">
-			<p class="text-gray-500">© 2019-2021 All Rights Reserved.</p>
-			<p class="font-medium text-gray-700">Terms of Service</p>
+			<p class="text-gray-300">© 2019-2021 All Rights Reserved.</p>
+			<p class="font-medium text-gray-300">Terms of Service</p>
 		</div>
 	</footer>
 </div>
+<style>
+		.wall {
+		background-image: url('images/wall2.jpg');
+		background-size: cover;
+		background-attachment: fixed;
+	}
+			.wall2 {
+		background-image: url('images/wall3.jpg');
+		background-size: cover;
+		background-attachment: fixed;
+	}
+	.wall3{
+		background-image: url('images/wall.jpg');
+		background-size: cover;
+		background-attachment: fixed;
+	}
+	.wall4 {
+				background-image: url('images/wall4.jpg');
+		background-size: cover;
+		background-attachment: fixed;
+	}
+</style>
